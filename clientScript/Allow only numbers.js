60f3fcc03ad1080bdbc28c1b861bb2c2188d4701
Everlyn <45017737+everlynyamamoto@users.noreply.github.com>
2020-09-23 17:50:53 -0300

    Update allow only numbers

--- clientScript/Allow only numbers.js
+++ clientScript/Allow only numbers.js
@@ -2,10 +2,11 @@ function onChange(control, oldValue, newValue, isLoading) {
     if (isLoading || newValue == '') {
         return;
     }
-    var numericVal = g_form.getValue('u_branch');
+    //validação de campo onde só é permitido números e não pode haver mais do que 4 digitos
+    var numericVal = g_form.getValue('field');
     if (isNaN(numericVal) || (numericVal.length > 4)) {
-        g_form.addInfoMessage(getMessage('this.field.must.be.four.digits'));
-        g_form.clearValue('u_branch');
+        g_form.addInfoMessage('This field must be 4 digits ');
+        g_form.clearValue('field');
     }
     
 }
